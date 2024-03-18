253397675d8efe085dc6b9a2418679f0c47b266d
ProgramWithGraham <gaps512@gmail.com>
2024-03-17 21:01:08 -0500

    got donut counter to work

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let counter = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
   const developerLink = document.getElementById("developer-dropdown");
   const developerModal = document.getElementById("developer-modal");
   const companyInfoLink = document.getElementById("company-info-link");
   const companyInfoModal = document.getElementById("company-info-modal");
+  let donutCount = document.getElementById("donut-count");
+  let donutButton = document.getElementById("donut-button");
 
   developerLink.addEventListener("click", function () {
     developerModal.style.display = "block";
@@ -10,6 +14,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   companyInfoLink.addEventListener("click", function () {
     companyInfoModal.style.display = "block";
+  });
+  donutButton.addEventListener("click", function () {
+    counter++;
+    donutCount.innerHTML = counter;
   });
 
   // Close modals when clicking outside of them
@@ -23,9 +31,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-for (let i = 0; i < 1000; i++) {
-  document.querySelector("#donuts").click();
+function countUp() {
+  const bigHeader = document.getElementById("donut-count");
+
+  console.log(bigHeader, typeof bigHeader, "bigHeader");
+
+  bigHeader.innerText = counter;
 }
 
-const container = (document.querySelector(".container").innerText =
-  "mmmmmmmmm doughnuts.... ahhhhhhh!");
+setInterval(countUp, 100);
+
+// let button = document.getElementById("donut-button");
+// let countDisplay = document.getElementById("donut-count");
+// button.addEventListener("click", function () {
+//   counter++;
+//   countDisplay.innerHTML = counter;
+// });
+
+for (let i = 0; i < 1000; i++) {
+  document.querySelector("cookie-header").click();
+}
